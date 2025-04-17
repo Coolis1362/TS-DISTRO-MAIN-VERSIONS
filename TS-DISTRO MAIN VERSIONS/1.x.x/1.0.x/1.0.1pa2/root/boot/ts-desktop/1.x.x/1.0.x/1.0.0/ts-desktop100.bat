f95a6631dfa95caa5b9938f31d962eb4430195ce
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-04-17 17:22:47 -0400

    Update ts-desktop100.bat

--- TS-DISTRO MAIN VERSIONS/1.x.x/1.0.x/1.0.1pa2/root/boot/ts-desktop/1.x.x/1.0.x/1.0.0/ts-desktop100.bat
+++ TS-DISTRO MAIN VERSIONS/1.x.x/1.0.x/1.0.1pa2/root/boot/ts-desktop/1.x.x/1.0.x/1.0.0/ts-desktop100.bat
@@ -6,3 +6,8 @@ cd /d "%~dp0"
 start cmd /k ts-desktop100.exe
 
 pause
+
+:: Check if the first argument is --version
+if "%1"=="--version" (
+    echo TS-DESKTOP FOR TS-KERNEL 1.0.1pa2 to TS-KERNEL 1.0.1pa7 VERSION: 1.0.0
+)
