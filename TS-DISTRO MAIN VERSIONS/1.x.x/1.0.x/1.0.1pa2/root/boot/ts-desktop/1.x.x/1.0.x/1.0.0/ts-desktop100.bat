37bd03b125b85e456056bf51e2fd6c3be2b49f5d
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-04-17 17:27:22 -0400

    update ts-desktop100.bat

--- TS-DISTRO MAIN VERSIONS/1.x.x/1.0.x/1.0.1pa2/root/boot/ts-desktop/1.x.x/1.0.x/1.0.0/ts-desktop100.bat
+++ TS-DISTRO MAIN VERSIONS/1.x.x/1.0.x/1.0.1pa2/root/boot/ts-desktop/1.x.x/1.0.x/1.0.0/ts-desktop100.bat
@@ -1,13 +1,17 @@
 @echo off
+
+:: Check if the first argument is --version
+if "%1"=="--version" (
+    echo TS-DESKTOP FOR TS-KERNEL 1.0.1pa2 to TS-KERNEL 1.0.1pa7 VERSION: 1.0.0
+)
+
+:: Start ts-desktop100.exe If Without Argument
 cls
 REM Change to the directory where the .bat file is located
 cd /d "%~dp0"
 
 start cmd /k ts-desktop100.exe
 
-:: Check if the first argument is --version
-if "%1"=="--version" (
-    echo TS-DESKTOP FOR TS-KERNEL 1.0.1pa2 to TS-KERNEL 1.0.1pa7 VERSION: 1.0.0
-)
+
 
 pause
